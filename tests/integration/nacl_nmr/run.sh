1ee4b8077e25a574dfae45dbe9be68259d0cf262
Moritz Gubler <moritz.gubler@e.email>
2026-03-02 12:12:01 +0100

    smaller test, run in parallel and without mpi

--- tests/integration/nacl_nmr/run.sh
+++ tests/integration/nacl_nmr/run.sh
@@ -5,13 +5,14 @@ rm -rf *.out scratch
 
 mpirun -np 4 $PW -in pw.scf.in > scf.out 2>&1
 
-mpirun -np 4 $QECONVERSE < Na1x.in > Na1x.out
-mpirun -np 4 $QECONVERSE < Na1y.in > Na1y.out
-mpirun -np 4 $QECONVERSE < Na1z.in > Na1z.out
+$QECONVERSE < Na1x.in > Na1x.out &
+$QECONVERSE < Na1y.in > Na1y.out &
+$QECONVERSE < Na1z.in > Na1z.out &
 
-mpirun -np 4 $QECONVERSE < Cl2x.in > Cl2x.out
-mpirun -np 4 $QECONVERSE < Cl2y.in > Cl2y.out
-mpirun -np 4 $QECONVERSE < Cl2z.in > Cl2z.out
+$QECONVERSE < Cl2x.in > Cl2x.out &
+$QECONVERSE < Cl2y.in > Cl2y.out &
+$QECONVERSE < Cl2z.in > Cl2z.out &
+wait
 
 python3 ../check_nmr.py \
     --files Na1x.out Na1y.out Na1z.out Cl2x.out Cl2y.out Cl2z.out \
